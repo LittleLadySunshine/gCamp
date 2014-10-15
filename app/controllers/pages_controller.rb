f604e25241a3ea373096a2d934e658de7a366b4b
Denise McCoy <denisemccoy@Denises-MacBook-Air.local>
2014-10-15 14:13:15 -0600

    ruby quotes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,21 @@
 class PagesController <ApplicationController
 
   def index
-    @quotes = [
-      ['"Keep your eyes on the stars, and your feet on the ground."', "Theodore Roosevelt"],
-      ['"Start where you are. Use what you have. Do what you can."', "Arthur Ashe"],
-      ['"A creative man is motivated by the desire to achieve, not by the desire to beat others."',"Ayn Rand"]
-]
+
+
+    quote1 = Quote.new
+    quote1.text = '"Keep your eyes on the stars, and your feet on the ground."'
+    quote1.author = "Theodore Roosevelt"
+
+    quote2 = Quote.new
+    quote2.text = '"Start where you are. Use what you have. Do what you can."'
+    quote2.author = "Arthur Ashe"
+
+    quote3 = Quote.new
+    quote3.text = '"A creative man is motivated by the desire to achieve, not by the desire to beat others."'
+    quote3.author = "Ayn Rand"
+
+  @quotes = [quote1, quote2, quote3]
   end
 
 end
