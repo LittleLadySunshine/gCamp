05057f75d26c3da41d16a6f98b62f1e90a563e94
Denise McCoy <denisemccoy@Denises-MacBook-Air.local>
2014-10-15 13:40:12 -0600

    refactored quotes into controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,11 @@
 class PagesController <ApplicationController
 
+  def index
+    @quotes = [
+      ['"Keep your eyes on the stars, and your feet on the ground."', "Theodore Roosevelt"],
+      ['"Start where you are. Use what you have. Do what you can."', "Arthur Ashe"],
+      ['"A creative man is motivated by the desire to achieve, not by the desire to beat others."',"Ayn Rand"]
+]
+  end
+
 end
