3b53e15ee2d64c5dfcab582301b1bc701f98b6be
Denise McCoy <Denise.L.McCoy@gmail.com>
2014-11-14 15:28:22 -0700

    afterrever

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,12 +1,3 @@
 class Task < ActiveRecord::Base
   validates :description, presence: true
-
-  validate :not_past_date
-
-  def not_past_date
-    if due_date.present? && due_date < Date.today
-      errors.add(:due_date, "cannnot be in the past")
-    end
-  end
-
 end
--- spec/features/project_spec.rb
+++ spec/features/project_spec.rb
@@ -1,14 +1,14 @@
-require 'rails_helper'
-
-feature "Project" do
-
-  scenario "User creates a project" do
-    visit projects_path
-    expect(page).to have_no_content("My awesome name")
-    click_on "Create Project"
-    fill_in "Name", with: "My awesome name"
-    click_on "Create Project"
-
-    expect(page).to have_content("My awesome name")
-  end
-end
+# require 'rails_helper'
+#
+# feature "Project" do
+#
+#   scenario "User creates a project" do
+#     visit projects_path
+#     expect(page).to have_no_content("My awesome name")
+#     click_on "Create Project"
+#     fill_in "Name", with: "My awesome name"
+#     click_on "Create Project"
+#
+#     expect(page).to have_content("My awesome name")
+#   end
+# end
