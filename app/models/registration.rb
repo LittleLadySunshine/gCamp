f589661f3c198e25637602568f945789193b12e3
Denise McCoy <Denise.L.McCoy@gmail.com>
2014-11-18 10:42:27 -0700

    sign-up validations work

--- app/models/registration.rb
+++
@@ -1,7 +0,0 @@
-class Registration < ActiveRecord::Base
-
-  validates :first_name, presence: true, :last_name, presence: true
-   :email, presence: true, uniqueness: { case_sensitive: false }
-  has_secure_password
-
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 
-  validates :email, presence: true, uniqueness: true, case_sensitive: false
+  # validates :email, presence: true, uniqueness: true, case_sensitive: false
+  validates_presence_of :first_name, :last_name, :email
   has_secure_password
 
 end
