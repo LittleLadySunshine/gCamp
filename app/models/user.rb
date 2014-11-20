85552606ce0f8ad3b12f37e136236652bdee34bd
Denise McCoy <Denise.L.McCoy@gmail.com>
2014-11-20 11:46:16 -0700

    membershipspelledright

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   validates_presence_of :first_name, :last_name, :email
   has_secure_password
   has_many :memberships
+  has_many :projects, through: :memberships
 
 end
