a2df3422d9e0f5cd7cb8d2ab9f3a1497ae1a09f4
Denise McCoy <Denise.L.McCoy@gmail.com>
2014-11-21 12:42:14 -0700

    description link

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,16 +1,16 @@
 class Task < ActiveRecord::Base
+
+  belongs_to :project
+
   validates :description, presence: true
-    belongs_to :project
 
 
-  validate :not_past_date
+  validate :due_date_valid
 
-  def not_past_date
-    if due_date.present? && due_date < Date.today
-      errors.add(:due_date, "cannnot be in the past")
-
+  def due_date_valid
+    if due_date.present? && due_date < due_date.current && !id.present?
+      errors.add(:due_date, "must be present or future")
     end
   end
-
 
 end
