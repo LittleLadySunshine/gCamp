39ac6715544202994cb2fd1d04667d945eb301f3
Denise McCoy <Denise.L.McCoy@gmail.com>
2014-11-25 15:24:35 -0700

    database fix

--- db/migrate/20141120164245_comments_table.rb
+++ db/migrate/20141120164245_comments_table.rb
@@ -1,9 +1,6 @@
 class CommentsTable < ActiveRecord::Migration
-  def change
-    create_table :comments do |t|
-      t.integer :task_id
-      t.integer :user_id
-      t.string :text
+    def change
+      drop_table :comments
     end
-  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141120164245) do
+ActiveRecord::Schema.define(version: 20141125195723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "comments", force: true do |t|
-    t.integer "task_id"
-    t.integer "user_id"
-    t.string  "text"
-  end
 
   create_table "memberships", force: true do |t|
     t.integer "user_id"
