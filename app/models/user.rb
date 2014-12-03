caaf933b8e7fe6f025b908d74ac85a09db56a4d2
Denise McCoy <Denise.L.McCoy@gmail.com>
2014-12-03 10:00:04 -0700

    deleted user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,15 @@ class User < ActiveRecord::Base
 
   # validates :email, presence: true, uniqueness: true, case_sensitive: false
 
+  validates_presence_of :first_name, :last_name, :email
+  has_secure_password
+  has_many :memberships, dependent: :destroy
+  has_many :projects, through: :memberships
+
   def full_name
     "#{first_name} #{last_name}"
   end
 
-  validates_presence_of :first_name, :last_name, :email
-  has_secure_password
-  has_many :memberships
-  has_many :projects, through: :memberships
+
 
 end
