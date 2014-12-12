9dfb73d882a84869f35dc29a356516e7ab5389c7
Denise McCoy <Denise.L.McCoy@gmail.com>
2014-12-12 08:33:44 -0700

    saving changes in my tracker show page

--- app/controllers/tracker_projects_controller.rb
+++ app/controllers/tracker_projects_controller.rb
@@ -1,3 +1,16 @@
 class TrackerProjects < ApplicationController
 
+  def show
+    tracker_api = TrackerAPI.new
+    @tracker_projects = tracker_api.projects(current_user.tracker_token)
+    @tracker_stories = tracker_api.stories(current_user.tracker_token, params[:id])
+
+    @name = "Test McCoy"
+    @tracker_projects.each do |project|
+      if params[:id] == project[:id]
+        @name = project[:name]
+      end
+    end
+  end
+
 end
